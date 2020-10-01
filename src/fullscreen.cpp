f9cfe7d43ca918fa04169b8916fc7684a7960255
Chris Olson <chris.m.olson@gmail.com>
2020-10-01 14:42:51 -0500

    Need to merge this into dev. (#9)
    * Change orignal name to match project
    
    * Delete editor.pro.user
    
    * Delete editor.pro
    
    * Created ignore file
    
    * Added menu dropdown status setting
    
    * Master (#4)
    
    * Updated the pro files to match the project file (#1)
    
    * Change orignal name to match project
    
    * Delete editor.pro.user
    
    * Delete editor.pro
    
    * Added .gitignore (#2)
    
    * Change orignal name to match project
    
    * Delete editor.pro.user
    
    * Delete editor.pro
    
    * Created ignore file
    
    * Set up properly and added menu dropdown status updates (#3)
    
    * Change orignal name to match project
    
    * Delete editor.pro.user
    
    * Delete editor.pro
    
    * Created ignore file
    
    * Added menu dropdown status setting
    
    * Update README.md
    
    First real version
    
    * Update README.md
    
    Fixed awful spelling and grammar errors and included a link to scrivner on the first mention
    
    * Most of the way toward getting find to work.
    
    * Adding icon
    
    * Added this user change
    
    * Fixed some bad dialog prompts
    Fixed the open to properly name unnamed scenes in the tree
    Fixed the find next child logic to properly visit the scene tree
    
    * Staged
    
    * 1) Implemented replace
    2) Added saving of dialog positions to novel
    3) Added saving of splitter positions to novel
    4) Added tool bar
    5) Added icons to menu actions
    
    * Added fullscreen commands, added about, added statement dialog
    
    * Added BookSmithPDF to project

--- src/fullscreen.cpp
+++ src/fullscreen.cpp
@@ -30,7 +30,44 @@ void FullScreen::keyReleaseEvent(QKeyEvent *e)
 {
     if (e->key() == Qt::Key_Escape) this->close();
 
-    // [TODO] handle all of the edit keys
+    QTextEdit* text = findChild<QTextEdit*>("textEdit");
+    QTextCursor postCursor = text->textCursor();
+    QTextCursor cursor = text->textCursor();
+    QTextBlockFormat blk = postCursor.blockFormat();
+    QTextCharFormat is = cursor.charFormat();
+    QTextCharFormat fmt;
+
+    if (e->matches(QKeySequence::Cut)) text->cut();
+    if (e->matches(QKeySequence::Copy)) text->copy();
+    if (e->matches(QKeySequence::Paste)) {
+        if (text->canPaste()) {
+            text->paste();
+            blk.setTextIndent(20.0);
+            blk.setBottomMargin(10.0);
+            postCursor.setBlockFormat(blk);
+            text->setTextCursor(postCursor);
+            MainWindow::getMainWindow()->change();
+        }
+    }
+    if (e->matches(QKeySequence::Bold)) {
+        fmt.setFontWeight(is.fontWeight() != QFont::Bold ? QFont::Bold : QFont::Normal);
+        cursor.mergeCharFormat(fmt);
+        text->mergeCurrentCharFormat(fmt);
+        MainWindow::getMainWindow()->change();
+    }
+    if (e->matches(QKeySequence::Italic)) {
+        fmt.setFontItalic(!is.fontItalic());
+        cursor.mergeCharFormat(fmt);
+        text->mergeCurrentCharFormat(fmt);
+        MainWindow::getMainWindow()->change();
+    }
+    if (e->matches(QKeySequence::Underline)) {
+        fmt.setFontUnderline(!is.fontUnderline());
+        cursor.mergeCharFormat(fmt);
+        text->mergeCurrentCharFormat(fmt);
+        MainWindow::getMainWindow()->change();
+    }
+    if (e->matches(QKeySequence::Save)) MainWindow::getMainWindow()->saveAction();
 }
 
 void FullScreen::textChangedAction()
